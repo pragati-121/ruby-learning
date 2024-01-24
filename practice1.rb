b914e2a72f3f913b37d184379a5f4d5023ab49df
Pragati Malviya <malviyapragati43@gmail.com>
2024-01-24 09:12:02 +0530

    saye hello world in 3 ways

--- practice1.rb
+++ practice1.rb
@@ -1,1 +1,18 @@
-ruby.rb
+#say hello world in multiple ways
+#Simple put string
+#using a variable
+#using a method
+
+puts "Hello World"
+
+greetings = "Hello World"
+#variable is greeting and vairable is basically a address refreing to some memory and what ever we have put in that memory
+#here greeting is a like address which the refrece of hello world
+puts greetings
+
+
+def say_hello(thing_to_say)
+	puts  thing_to_say
+end
+
+say_hello "Hello Pragati"
