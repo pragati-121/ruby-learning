8ddc5a765a52d977dc28e27288c0ac8291e8e128
Pragati Malviya <malviyapragati43@gmail.com>
2024-01-24 09:43:42 +0530

    common methods of strings

---
+++ methods.rb
@@ -0,0 +1,9 @@
+puts 10.class
+
+full_name = "Pragati Malviya"
+puts full_name.length
+puts full_name.reverse
+puts full_name.empty?
+
+sentence = "Welcome to the Safari"
+puts sentence.sub("Safari", "India")
