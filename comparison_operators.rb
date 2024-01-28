69e8e1f6c6260cba347bb01ded897427a3fbcd88
Pragati Malviya <malviyapragati43@gmail.com>
2024-01-28 21:51:14 +0530

    learned aout method

---
+++ comparison_operators.rb
@@ -0,0 +1,7 @@
+a= 5
+b=6
+puts a
+puts b 
+puts a==b #this will gives false
+puts 10 == 10.to_f #this will give true
+puts 100 >= 100
---
+++ methods_learining.rb
@@ -0,0 +1,31 @@
+puts "Enter First Number"
+num1 = gets.chomp
+
+puts "Enter Second Number"
+num2 = gets.chomp
+
+def multiply(first_num, second_num)
+  first_num.to_i * second_num.to_i
+end
+ 
+def addition(first_num, second_num)
+  first_num.to_i + second_num.to_i
+end
+
+def subtraction(first_num, second_num)
+  first_num.to_i - second_num.to_i
+end
+
+def division(first_num, second_num)
+  first_num.to_i / second_num.to_i
+end
+
+def modulo(first_num, second_num)
+  first_num.to_i % second_num.to_i
+end
+
+puts "The first number multiplied with the second number is #{multiply( num1, num2) }"
+puts "The first number added with the second number is #{ addition( num1, num2)}"
+puts "The first number subtracted with the second number is #{ subtraction( num1, num2) }"
+puts "The first number divided with the second number is #{ division( num1, num2)}"
+puts "The first number modules with the second number is #{ modulo( num1, num2)}"
