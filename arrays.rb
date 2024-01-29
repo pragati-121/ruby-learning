1a09f7482a1946f6d1ba470c8438cc37ffdc664d
Pragati Malviya <malviyapragati43@gmail.com>
2024-01-29 15:42:52 +0530

    arrays working'

---
+++ arrays.rb
@@ -0,0 +1,24 @@
+#Array is an odered list of itme and each element in an array can be anything string or number or hash
+# a=  [1,2,3,4,5]
+#puts a
+# print a
+# puts
+# p a
+
+
+#to access any item in an array
+
+# p a.last
+# p a.first
+# x = (1..10).to_a
+# p x
+# p x.reverse
+# p x.reverse!
+#! mutate
+
+
+b = "a".."z"
+# p b.to_a
+# p b.to_a.shuffle
+
+#<< shovel operator
